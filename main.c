7c30c68e265b4cbbc8d3833f72ce074dc102f432
Kaporos <theo@daron.be>
2024-02-14 19:21:03 +0100

    being clearer about demo

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ void greatTest()
     int b = 4.0 == 5.0;
     printf("some debug inside the test\n");
     printf("another debug\n");
+    // theses assert are MEANT to fail.
     t_assert_int(5, ==, 6);
     t_assert_int(5, ==, 8);
 }
